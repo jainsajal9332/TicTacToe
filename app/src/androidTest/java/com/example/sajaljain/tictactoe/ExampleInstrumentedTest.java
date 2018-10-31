176d2e0a2d37b2a863688d0463f784b59eac286e
prince721 <mishraprince721@gmail.com>
2018-10-31 12:11:58 +0530

    Bugs fixed
    Added commnets

--- app/src/androidTest/java/com/example/sajaljain/tictactoe/ExampleInstrumentedTest.java
+++ app/src/androidTest/java/com/example/sajaljain/tictactoe/ExampleInstrumentedTest.java
@@ -8,12 +8,7 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 
 import static org.junit.Assert.*;
-
-/**
- * Instrumented test, which will execute on an Android device.
- *
- * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
- */
+// Comment removed
 @RunWith(AndroidJUnit4.class)
 public class ExampleInstrumentedTest {
     @Test
